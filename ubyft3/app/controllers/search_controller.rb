fcf610810a739a2e617da07ea616893b93639174
Pignataro67 <Pignataro67@gmail.com>
2019-10-09 12:28:25 -0700

    adding Search cont.

---
+++ ubyft3/app/controllers/search_controller.rb
@@ -0,0 +1,14 @@
+class SearchController < ApplicationController
+
+  def search_destination
+    input = params[:input]
+    response = RestClient::Request.execute(
+    method: :get,
+    url: "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=#{input}&key=#{ENV['GOOGLE_PLACES_KEY']}",
+    )
+    render json: response
+  end
+  
+  def drop_off
+    end
+  end 
