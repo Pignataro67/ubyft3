c7d6007cd2d13a9d2a8b1ef598371ccbfb01c82d
Pignataro67 <Pignataro67@gmail.com>
2019-10-10 12:56:36 -0700

    refactoring

--- ubyft3/app/controllers/search_controller.rb
+++ ubyft3/app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
   def search_destination
     input = params[:input]
-    response = RestClient::Request.execute(
+    res = RestClient::Request.execute(
     method: :get,
     url: "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=#{input}&key=#{ENV['GOOGLE_PLACES_KEY']}",
     )
@@ -10,5 +10,6 @@ class SearchController < ApplicationController
   end
   
   def drop_off
+    
     end
   end 
