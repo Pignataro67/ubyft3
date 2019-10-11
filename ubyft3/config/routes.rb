9b25ea406e138723f5b2262132c77091d6ffbe2c
Pignataro67 <Pignataro67@gmail.com>
2019-10-11 09:42:55 -0700

    adding reducers, improving routes

--- ubyft3/config/routes.rb
+++ ubyft3/config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope(:path => '/RailsApi') do
+    get '/search_destination/:input', to: 'search#search_destination'
+    get '/confirm_route/convert_coords/:location', to: 'confirm_route#get_coords'
+    get '/confirm_route/mapbox', to: 'confirm_route#mapbox'
+    get '/uber', to: 'uber_lyft#uber_estimate'
+    get '/lyft', to: 'uber_lyft#lyft_estimate'
+  end
 end
